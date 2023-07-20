54e4fd721f885873b2d99f16a456d0f0d1e0c6c3
Neem48 <65356295+Neem48@users.noreply.github.com>
2023-07-20 16:11:18 +0100

    Update iris_app.py

--- iris_app.py
+++ iris_app.py
@@ -5,6 +5,9 @@ import seaborn as sns
 import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
 iris_df = pd.read_csv("iris-species.csv")
 iris_df['Label'] = iris_df['Species'].map({'Iris-setosa': 0, 'Iris-virginica': 1, 'Iris-versicolor':2})
 X = iris_df[['SepalLengthCm','SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']]
@@ -14,11 +17,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, rand
 
 svc_model = SVC(kernel = 'linear')
 svc_model.fit(X_train, y_train)
-score = svc_model.score(X_train, y_train)
+rf_clf = RandomForestClassifier(n_jobs = -1, n_estimators = 100)
+rf_clf.fit(X_train, y_train)
+log_reg = LogisticRegression(n_jobs = -1)
+log_reg.fit(X_train, y_train)
 
 @st.cache()
-def prediction(sepal_length, sepal_width, petal_length, petal_width):
-  species = svc_model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+def prediction(model,sepal_length, sepal_width, petal_length, petal_width):
+  species = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
   species = species[0]
   if species == 0:
     return "Iris-setosa"
@@ -33,7 +39,16 @@ s_len = st.sidebar.slider("Sepal Length", 0.0, 10.0)
 s_wid = st.sidebar.slider("Sepal Width", 0.0, 10.0)
 p_len = st.sidebar.slider("Petal Length", 0.0, 10.0)
 p_wid = st.sidebar.slider("Petal Width", 0.0, 10.0)
+classifier = st.sidebar.selectbox("Classifier type:", ("Support Vector Machine", "Logistic Regression", "Random Forest Classifier")
 if st.sidebar.button("Predict"):
-	species_type = prediction(s_len, s_wid, p_len, p_wid)
+	if classifier == "Support Vector Machine":
+        	species_type = prediction(svc_model,s_len, s_wid, p_len, p_wid)
+		score = svc_model.score(X_train, y_train)
+	elif classifier == "Logistic Regression":
+		species_type = prediction(log_reg,s_len, s_wid, p_len, p_wid)
+		score = log_reg.score(X_train, y_train)
+	else:
+		species_type = prediction(rf_clf,s_len, s_wid, p_len, p_wid)
+		score = rf_clf.score(X_train, y_train)
 	st.write("Species predicted:", species_type)
 	st.write("Accuracy score of this model is:", score)
